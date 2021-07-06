d49a6d9313065db80d57dcbc0620e5b97931dd90
Raya Sodwilai <sodwilai@pdx.edu>
2021-07-06 12:44:36 -0700

    add signin functionality

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,27 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ticket from "./Ticket";
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded, isEmpty, constants, reactReduxFirebase } from 'react-redux-firebase'
 
 function TicketList(props){
-  return (
-    <React.Fragment>
+  useFirestoreConnect([
+    { collection: 'tickets' }
+  ]);
+
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  if (isLoaded(tickets)) {
+    return (
+      <React.Fragment>
       <hr/>
-      {Object.values(props.ticketList).map((ticket) => {
-        return <Ticket
-          whenTicketClicked = { props.onTicketSelection }
-          names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          formattedWaitTime={ticket.formattedWaitTime}
-          id={ticket.id}
-          key={ticket.id}/>
-      })}
-    </React.Fragment>
-  );
+        {tickets.map((ticket) => {
+          return <Ticket
+            whenTicketClicked = { props.onTicketSelection }
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            formattedWaitTime={ticket.formattedWaitTime}
+            id={ticket.id}
+            key={ticket.id}/>
+        })}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <react.Fragment>
+        <h3>Loading...</h3>
+      </react.Fragment>
+    )
+  }
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
